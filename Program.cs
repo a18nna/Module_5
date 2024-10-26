c84f68aa918a5426092ce5171dbb0a1e1cf8ce8a
al18nna <melnik.alena2006@gmail.com>
2024-10-26 14:27:52 +0300

    Задание 5.2.7

--- Program.cs
+++ Program.cs
@@ -8,17 +8,65 @@ namespace exercise5_1
 {
     internal class Program
     {
+        static string ShowColor(string userName, int userAge)
+        {
+            Console.WriteLine(userName + ", " + userAge + "\n Введите свой любимый цвет: ");
+            string color = Console.ReadLine().ToLower();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine($"Your color is {color}!");
+                    break;
+            }
+            return color;
+        }
         static void Main(string[] args)
         {
-            (string name, string[] dishes) User;
-            Console.WriteLine("Введите имя пользователя: ");
-            User.name = Console.ReadLine();
-            User.dishes = new string[5];
-            for (int i = 0; i < 5; i++)
+            var (name, age) = ("Алена", 18);
+            Console.WriteLine("Моё имя: " + name + "\n" + "Мой возраст: " + age);
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: " + name + " \n" + "Ваш возраст: " + age);
+
+            string[] favColors = new string[3];
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine($"Введите любимое блюдо номер {i + 1}: ");
-                User.dishes[i] = Console.ReadLine();
+                favColors[i] = ShowColor(name, age);
+                Console.WriteLine(favColors[i]);
             }
+
+            Console.WriteLine("Три ваших любимых цвета: ");
+            foreach (var i in favColors)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
         }
     }
 }
