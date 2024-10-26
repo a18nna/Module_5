53e341b10e6e5f89359963d108373de50949d27e
al18nna <melnik.alena2006@gmail.com>
2024-10-26 17:57:20 +0300

    Задание 5.3.3

--- Program.cs
+++ Program.cs
@@ -8,96 +8,28 @@ namespace exercise5_1
 {
     internal class Program
     {
-        static int[] GetArrayFromConsole(int num = 5)
+        static void GetName(ref string name)
         {
-            var result = new int[num];
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
-            }
-            return result;
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
         }
 
-        static int[] SortArray(int[] array)
+        static int ShangeAge(int age)
         {
-            int temp = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-            return array;
-        }
-
-        static void ShowArray(int[] array, bool IsSort = false)
-        {
-            var temp = array;
-            if (IsSort)
-            {
-                temp = SortArray(temp);
-            }
-            foreach (var sort in array)
-            {
-                Console.WriteLine(sort);
-            }
-        }
-
-        static void ShowColors(string userName, params string[] favColors)
-        {
-            Console.WriteLine($"{userName}, ваши любимые цвета: ");
-            foreach (var color in favColors)
-            {
-                Console.WriteLine(color);
-            }
-        }
-
-        static string ShowColor(string userName, int userAge)
-        {
-            Console.WriteLine(userName + ", " + userAge + "\n Введите свой любимый цвет: ");
-            string color = Console.ReadLine().ToLower();
-            switch (color)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine($"Your color is {color}!");
-                    break;
-            }
-            return color;
+            age = age + 10;
+            return age;
         }
         static void Main(string[] args)
         {
-            int[] array = GetArrayFromConsole(10);
-            ShowArray(array, true);
+            Console.WriteLine("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            ShangeAge(age);
+
+            Console.WriteLine(age);
+
+            string name = Console.ReadLine();
+            GetName(ref name);
+            Console.WriteLine(name);
 
             Console.ReadKey();
         }
